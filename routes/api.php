63e67b46278d376f97da0475a517a6459f2bc126
MekaellaGojit <mekaella.gojit@sorsu.edu.ph>
2025-12-04 03:30:58 +0800

    Add migration route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SaleController;
 use App\Http\Controllers\AuthController;
